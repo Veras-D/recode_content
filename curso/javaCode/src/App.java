c330661ada78cbdb7aa6911579c8793674b30b19
Veras-D <dveras2310@gmail.com>
2024-12-23 17:36:12 -0300

    studding java

---
+++ curso/javaCode/src/App.java
@@ -0,0 +1,24 @@
+package src;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("It is funny xD");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int numTest = scanner.nextInt();
+        System.out.println("The number is: " + numTest);
+
+        /* 
+         * calculator(a, b, operator)
+         * função validar a, b
+         * sum
+         * sub
+         * mul
+         * div
+         * rad
+         * pot
+        */
+    }
+}
