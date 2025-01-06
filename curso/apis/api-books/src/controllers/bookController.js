f24e01d9191d85b73d2828db1362fd09b53aa38a
Veras-D <dveras2310@gmail.com>
2025-01-05 23:50:43 -0300

    corrections in book manager project

--- curso/apis/api-books/src/controllers/bookController.js
+++ curso/apis/api-books/src/controllers/bookController.js
@@ -1,12 +1,70 @@
-const express = require('express');
-const bookController = require('./controllers/bookController');
+const bookModel = require('../models/bookModel');
 
-const router = express.Router();
+const getBooks = (req, res) => {
+    const books = bookModel.getAllBooks();
+    res.json(books);
+};
 
-router.get('/', bookController.getBooks);
-router.get('/:id', bookController.getBookById);
-router.post('/', bookController.createBook);
-router.put('/;id', bookController.updateBook);
-router.delete('/:id', bookController.deleteBook);
+const getBookById = (req, res) => {
+    const { id } = req.params;
+    const book = bookModel.getBookById(Number(id));
+    
+    if (book) {
+        res.json(book);
+    } else {
+        res.status(404).json({ message: 'Book not found' });
+    }
+};
 
-module.exports = router;
+const createBook = (req, res) => {
+    const { title, author, year} = req.body;
+    const books = bookModel.getAllBooks();
+    const newId = books.length > 1 ? books[books.length - 1].id + 1 : 1;
+
+    const newBook = { 
+        id: newId,
+        title,
+        author,
+        year 
+    };
+
+    const createdBook = bookModel.addBook(newBook);
+
+    if (createdBook) {
+        res.json(newBook);
+    } else {
+        res.status(400).json({ message: 'Error, unable to create book.' });
+    }
+};
+
+
+const updateBook = (req, res) => {
+    const { id } = req.params;
+    const updateData = req.body;
+    const updatedBook = bookModel.updateBook(Number(id), updateData);
+
+    if (updatedBook) {
+        res.json(updatedBook);
+    } else {
+        res.status(404).json({ message: 'Book not found' });
+    }
+};
+
+const deleteBook = (req, res) => {
+    const { id } = req.params;
+    const deletedBook = bookModel.deleteBook(Number(id));
+
+    if (deletedBook) {
+        res.json(deletedBook);
+    } else {
+        res.status(404).json({ message: 'Book not found' });
+    }
+}
+
+module.exports = {
+    getBooks,
+    getBookById,
+    createBook,
+    updateBook,
+    deleteBook,
+};
--- curso/apis/api-books/src/routers/bookRoutes.js
+++ curso/apis/api-books/src/routers/bookRoutes.js
@@ -1,70 +1,12 @@
-const bookModel = require('./models/bookModel');
+const express = require('express');
+const bookController = require('../controllers/bookController');
 
-const getBooks = (req, res) => {
-    const books = bookModel.getBooks();
-    res.json(books);
-};
+const router = express.Router();
 
-const getBookById = (req, res) => {
-    const { id } = req.params;
-    const book = bookModel.getBookById(Number(id));
-    
-    if (book) {
-        res.json(book);
-    } else {
-        res.status(404).json({ message: 'Book not found' });
-    }
-};
+router.get('/', bookController.getBooks);
+router.get('/:id', bookController.getBookById);
+router.post('/', bookController.createBook);
+router.put('/:id', bookController.updateBook);
+router.delete('/:id', bookController.deleteBook);
 
-const createBook = (req, res) => {
-    const { title, author, year} = req.body;
-    const books = bookModel.getBooks();
-    const newId = books.length > 1 ? books[books.length - 1].id + 1 : 1;
-
-    const newBook = { 
-        id: newId,
-        title,
-        author,
-        year 
-    };
-
-    const createdBook = bookModel.addBook(newBook);
-
-    if (createdBook) {
-        res.json(newBook);
-    } else {
-        res.status(400).json({ message: 'Error, unable to create book.' });
-    }
-};
-
-
-const updateBook = (req, res) => {
-    const { id } = req.params;
-    const updateData = req.body;
-    const updatedBook = bookModel.updateBook(Number(id), updateData);
-
-    if (updatedBook) {
-        res.json(updatedBook);
-    } else {
-        res.status(404).json({ message: 'Book not found' });
-    }
-};
-
-const deleteBook = (req, res) => {
-    const { id } = req.params;
-    const deletedBook = model.Book.deleteBook(Number(id));
-
-    if (deletedBook) {
-        res.json(deletedBook);
-    } else {
-        res.status(404).json({ message: 'Book not found' });
-    }
-}
-
-module.exports = {
-    getBooks,
-    getBookById,
-    createBook,
-    updateBook,
-    deleteBook,
-};
+module.exports = router;
